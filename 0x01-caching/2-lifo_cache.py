499076066fbcfd95fc2ab4e7d636f6f03c5b2a9c
Abrham Endale <abrhamendale9@gmail.com>
2023-05-16 17:29:34 +0000

    A fix for task_2.

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -24,18 +24,20 @@ class LIFOCache(BaseCaching):
         """
         Adds to the dictionary.
         """
-        if key not in self.key_order and len(self.cache_data) >= self.MAX_ITEMS:
-            for i in range(len(self.last_key) - 1, 0, -1):
-                if self.last_key[i] == 1:
-                    self.last_key[i] = 0
-                    del self.cache_data[self.key_order[i]]
-                    print("DISCARD:", self.key_order[i])
-                    break
+        if key not in self.key_order:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                for i in range(len(self.last_key) - 1, 0, -1):
+                    if self.last_key[i] == 1:
+                        self.last_key[i] = 0
+                        del self.cache_data[self.key_order[i]]
+                        print("DISCARD:", self.key_order[i])
+                        break
 
         if key and item:
             if self.cache_data is None:
                 self.lpush = key
             self.cache_data[key] = item
+            """if key not in self.key_order:"""
             self.key_order.append(key)
             self.last_key.append(1)
 
