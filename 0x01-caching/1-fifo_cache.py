4a8b4638649fddda90fd46ac7e40b737849359b5
Abrham Endale <abrhamendale9@gmail.com>
2023-05-16 17:02:06 +0000

    A fix for task_1.

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -24,20 +24,22 @@ class FIFOCache(BaseCaching):
         """
         Adds to the dictionary.
         """
-        if key not in self.key_order and len(self.cache_data) >= self.MAX_ITEMS:
-            for i in range(0, len(self.last_key)):
-                if self.last_key[i] == 1:
-                    self.last_key[i] = 0
-                    del self.cache_data[self.key_order[i]]
-                    print("DISCARD:", self.key_order[i])
-                    break
+        if key not in self.key_order:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                for i in range(0, len(self.last_key)):
+                    if self.last_key[i] == 1:
+                        self.last_key[i] = 0
+                        del self.cache_data[self.key_order[i]]
+                        print("DISCARD:", self.key_order[i])
+                        break
 
         if key and item:
             if self.cache_data is None:
                 self.lpush = key
             self.cache_data[key] = item
-            self.key_order.append(key)
-            self.last_key.append(1)
+            if key not in self.key_order:
+                self.key_order.append(key)
+                self.last_key.append(1)
 
     def get(self, key):
         """
