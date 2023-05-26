a28c66a332e043a242d60a83a07a3a90898e5088
Abrham Endale <abrhamendale9@gmail.com>
2023-05-26 19:58:03 +0000

    Added task_7.

---
+++ 0x02-i18n/7-app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Login module"""
+users = {
+        1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+        2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+        3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+        4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+        }
+
+
+def get_user():
+    """Returns user."""
+
+@app.before_request
+def before_request():
+    """
+    A function that runs before request
+    """
+
+@babel.timezoneselector
+def get_timezone():
+    """
+    Retrieves the time zone.
+    """
